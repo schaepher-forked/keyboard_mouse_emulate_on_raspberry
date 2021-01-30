ba4852e214917043e061486e9c938a994be856f9
schaepher <schaepher@outlook.com>
2021-01-30 17:30:13 +0800

    extract BTKbService

--- server/btk_server.py
+++ server/btk_server.py
@@ -107,46 +107,6 @@ class BTKbDevice():
             error(err)
 
 
-DBUS_OBJ_BTK_NAME = "org.thanhle.btkbservice"
-DBUS_OBJ_BTK_PATH = "/org/thanhle/btkbservice"
-
-class BTKbService(dbus.service.Object):
-
-    def __init__(self):
-        print("1. Setting up service")
-        # set up as a dbus service
-        bus_name = dbus.service.BusName(DBUS_OBJ_BTK_NAME, bus=dbus.SystemBus())
-        dbus.service.Object.__init__(
-            self, bus_name, "/org/thanhle/btkbservice")
-        # create and setup our device
-        self.device = BTKbDevice()
-        # start listening for connections
-        self.device.listen()
-
-    @dbus.service.method(DBUS_OBJ_BTK_NAME, in_signature='yay')
-    def send_keys(self, modifier_byte, keys):
-        print("Get send_keys request through dbus")
-        print("key msg: ", keys)
-        state = [ 0xA1, 1, 0, 0, 0, 0, 0, 0, 0, 0 ]
-        state[2] = int(modifier_byte)
-        count = 4
-        for key_code in keys:
-            if(count < 10):
-                state[count] = int(key_code)
-            count += 1
-        self.device.send_string(state)
-
-    @dbus.service.method(DBUS_OBJ_BTK_NAME, in_signature='yay')
-    def send_mouse(self, modifier_byte, keys):
-        state = [0xA1, 2, 0, 0, 0, 0]
-        count = 2
-        for key_code in keys:
-            if(count < 6):
-                state[count] = int(key_code)
-            count += 1
-        self.device.send_string(state)
-
-
 # main routine
 if __name__ == "__main__":
     # we an only run as root
